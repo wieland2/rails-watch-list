790f138e1911094064f85c15d22b649ccf918475
Wagner Wieland <wagnerp5@hotmail.com>
2023-02-27 17:01:36 -0300

    :)

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,2 +1,29 @@
 class BookmarksController < ApplicationController
+
+  def new
+    @bookmark = Bookmark.new
+    @list = List.find(params[:list_id])
+  end
+
+  def create
+    @bookmark = Bookmark.new(bookmarks_params)
+    @list = List.find(params[:list_id])
+    @bookmark.list_id = @list.id
+    if @bookmark.save
+    redirect_to list_path(@list)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @bookmark = Bookmark.find(params[:id])
+    @bookmark.destroy
+    redirect_to list_path(@bookmark.list), status: :see_other
+  end
+  private
+
+  def bookmarks_params
+    params.require(:bookmark).permit(:comment, :movie_id)
+  end
 end
